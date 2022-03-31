2fb3bba50469c4953c0f0c3ac848c8b032f3412e
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-31 16:20:00 -0700

    changed eslint rules and import data to app.js

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
 // import functions and grab DOM elements
-
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+import { data } from './data.js';
